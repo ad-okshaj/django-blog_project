a3816bfe9fa6f3ea658a6340c242e5cee03244a0
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2021-08-15 13:00:51 +0530

    added option to edit blog posts.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 # Create your views here.
 
 class BlogListView(ListView):
@@ -10,3 +11,13 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model=Post
     template_name='post_detail.html'
+
+class BlogCreateView(CreateView):
+    model=Post
+    template_name='post_new.html'
+    fields='__all__'
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    template_name="post_edit.html"
+    fields=['title','body']
