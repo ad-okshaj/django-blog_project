d344e697497ed4b332994a08f34f24cb8d120f70
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2021-08-15 13:36:00 +0530

    added tests to tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -42,3 +42,20 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post.new'), {
+            'title':'New title',
+            'body':'New text',
+            'author':self.user,
+            })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title') 
+        self.assertContains(response, 'New Text')
+
+    def test_post_update_view(self):
+        response=self.client.post(reverse('post_edit', args='1'),{
+            'title':'Updated title',
+            'body':'Updated text',
+        })
+        self.assertEqual(response.status_code, 302)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy 
+
 # Create your views here.
 
 class BlogListView(ListView):
@@ -21,3 +23,8 @@ class BlogUpdateView(UpdateView):
     model=Post
     template_name="post_edit.html"
     fields=['title','body']
+
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name='post_delete.html'
+    success_url=reverse_lazy('home')
