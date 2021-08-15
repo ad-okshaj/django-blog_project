c9e25fea212b23355371670483ae9b5ec02ba884
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2021-08-15 16:34:43 +0530

    trying to deploy app to heroku.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dlm_zqk7+@8+&m^@v1!$_fgsmbr+=fq**pz^c*cis1i0*+53)s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
